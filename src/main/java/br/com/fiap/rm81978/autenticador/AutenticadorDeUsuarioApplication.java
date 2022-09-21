1ec7fb91acc177e7624fa61567e09c5bea75ebdc
Lucas Amancio <lhamancio@gmail.com>
2022-09-21 20:21:35 -0300

    criando projeto

---
+++ src/main/java/br/com/fiap/rm81978/autenticador/AutenticadorDeUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.rm81978.autenticador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticadorDeUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticadorDeUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/rm81978/autenticador/AutenticadorDeUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.rm81978.autenticador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutenticadorDeUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
